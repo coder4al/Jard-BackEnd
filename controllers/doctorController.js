2d0083dc033d2ef81ee7cb4ed1245264e70eac6f
Abin-tek <abinathmouniane@gmail.com>
2025-05-07 19:07:46 +0530

    change-availability

---
+++ controllers/doctorController.js
@@ -0,0 +1,18 @@
+import doctorModel from "../models/doctorModel.js";
+
+const changeAvailability = async (req, res) => {
+  try {
+    const { docId } = req.body;
+
+    const docData = await doctorModel.findById(docId);
+    await doctorModel.findByIdAndUpdate(docId, {
+      available: !docData.available,
+    });
+    res.json({ success: true, message: "Availability Changed" });
+  } catch (error) {
+    console.error("Error during change availability: ", error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export default changeAvailability;
